b64a25fd3e138fa94d42c321bd32ffcf7fac1214
nijila <nijila.m@tiatech.net>
2022-03-26 20:17:21 +0530

    added gitignore

--- employee_master/settings.py
+++ employee_master/settings.py
@@ -161,7 +161,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 # User uploaded files (Images, Videos, PDF)
 # https://docs.djangoproject.com/en/2.2/topics/files/
 MEDIA_URL = '/media/'
