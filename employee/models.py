f4146ccafca18e46a227a21e483c5823ee44d32b
nijila <nijila.m@tiatech.net>
2022-03-26 21:58:22 +0530

    sgd

--- employee/models.py
+++ employee/models.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
 import datetime
 from phonenumber_field.modelfields import PhoneNumberField
 
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class User(AbstractUser):
     is_employee = models.BooleanField(default=False)
